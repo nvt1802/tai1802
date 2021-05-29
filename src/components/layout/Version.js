b68cf8a75e9696aff65f4b628e1df84db2529e22
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2021-05-29 15:14:31 +0700

    update

--- src/components/layout/Version.js
+++ src/components/layout/Version.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Version = () => {
   return (
-    <div style={{ position: 'absolute', bottom: '0.5em', left: '0.5em', opacity: 0.8, fontSize: '0.5em' }}>
+    <div style={{
+      position: 'absolute',
+      bottom: '0.5em',
+      left: '0.5em',
+      opacity: 0.8,
+      fontSize: '0.9em',
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+      textDecoration: 'underline'
+    }}>
       {`Ver: ${process.env?.REACT_APP_VERSION}`}
     </div>
   )
