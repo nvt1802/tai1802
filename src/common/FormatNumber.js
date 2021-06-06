11185ea5f326e56f283d5586edb77cde28435605
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2021-06-07 00:02:57 +0700

    update code

--- src/common/FormatNumber.js
+++
@@ -1,8 +0,0 @@
-class FormatNumber {
-  currencyformatter = new Intl.NumberFormat("vi-VN", {
-    // style: 'currency',
-    currency: "VND",
-  })
-}
-
-export default new FormatNumber()
--- src/services/district.service.js
+++
@@ -1,6 +0,0 @@
-import Axios from "axios"
-const BASE_URL = "https://vapi.vnappmob.com"
-
-export const findDistrictByProvinceId = (provinceId) => {
-  return Axios.get(`${BASE_URL}/api/province/district/${provinceId}`)
-}
